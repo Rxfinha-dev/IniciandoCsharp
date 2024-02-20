6411f7903096281e86d6f2666f9b20bc14188ef4
Usuario <Usuario@CPD03-M05>
2024-02-20 12:21:09 -0300

    Adicionar arquivos de projeto.

---
+++ Aula1DesenvolvimentoDeSistemas/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1DesenvolvimentoDeSistemas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá mundo");
+            Console.WriteLine("Tenha um bom dia");
+            Console.WriteLine(2 + 2 +" é o resultado.");
+            Console.WriteLine("O resultado é: " + (2 + 2));
+            Console.WriteLine(2.5);
+            Console.WriteLine((2 +2)*3);
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
